b4d183585cb8de967501831e4ae0add5808cbab1
Divyansh <divyanshkhandelwal147@gmail.com>
2020-06-18 23:32:20 +0530

    apidoc setup

---
+++ apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "Workbook",
+  "version": "0.1.0",
+  "description": "apiDoc basic example",
+  "apidoc": "0.3.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-06-18T17:59:53.985Z",
+    "url": "http://apidocjs.com",
+    "version": "0.23.0"
+  }
+});
---
+++ documentation/superAdmin.js
@@ -0,0 +1,32 @@
+/**
+ * @api {delete} /superAdmin/purge Purge Database
+ * @apiName PurgeDatabase
+ * @apiGroup Super Admin
+ *
+ *
+ *
+ */
+
+
+
+/**
+ * @api {post} /superAdmin/approveAdmin Approve Admin
+ * @apiName Approve Admin
+ * @apiGroup Super Admin
+ *
+ * @apiParam {String} id _id of the Admin Document
+ *
+ *
+ */
+
+
+
+/**
+ * @api {delete} /superAdmin/purge Request
+ * @apiName Purge Database
+ * @apiGroup Super Admin
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ *
+ */
