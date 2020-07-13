4f9559342172c0360550a073fe6c1a1acbda3f5e
Divyansh <divyanshkhandelwal147@gmail.com>
2020-07-13 16:15:22 +0530

    docs update

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -14,7 +14,7 @@ define({
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-07-12T18:58:46.149Z",
+    "time": "2020-07-13T10:45:05.194Z",
     "url": "http://apidocjs.com",
     "version": "0.23.0"
   }
